5ddf4411db3f9756189ed3b032f4a6736a48890b
Ronoyaro <ronaldferreiraig@hotmail.com>
2025-11-25 00:29:50 -0300

    feat: criei o metodo petSave

--- src/repository/PetRepository.java
+++ src/repository/PetRepository.java
@@ -4,58 +4,67 @@ import entities.Pet;
 import enums.Sexo;
 import enums.Tipo;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.attribute.FileTime;
-import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PetRepository {
+    private static List<String> archivesPet = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
-        savePet(new Pet("joao da", Tipo.CACHORRO, Sexo.FEMEA, List.of("9", "ssa", "br"), 5D, 6D, "baiano"));
+        save(new Pet("nick fury", Tipo.CACHORRO, Sexo.MACHO, List.of("9", "ssa", "brazil"), 5D, 6D, "vira lata"));
 
 
     }
 
-    private static Path createFilePet(Pet pet) throws IOException {
+    private static Path createFile(Pet pet) throws IOException {
         LocalDateTime now = LocalDateTime.now();
         String petNameArchive = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm")) + pet.getNome().replace(" ", "") + ".txt";
-        Path petPath = Paths.get(petNameArchive.toUpperCase());
+        Path petPath = Paths.get("src\\db_pets\\" + petNameArchive.toUpperCase());
         if (!Files.exists(petPath)) {
             return Files.createFile(petPath);
         }
-        throw new IOException("Arquivo já foi existe!");
+        throw new IOException("O Pet já existe!");
     }
 
-    public static void savePet(Pet pet) throws IOException {
-        Path filePet = createFilePet(pet);
+    public static void save(Pet pet) throws IOException {
+        Path filePet = createFile(pet);
         try (BufferedWriter br = Files.newBufferedWriter(filePet)) {
-            br.write("1 - "+pet.getNome());
+            String name = pet.getNome();
+            Tipo type = pet.getTipo();
+            Sexo sex = pet.getSexo();
+            List<String> endereco = pet.getEndereco();
+            Double age = pet.getIdade();
+            Double weight = pet.getPeso();
+            String race = pet.getRaca();
+            br.write("1 - " + name);
             br.newLine();
-            br.write("2 - "+pet.getTipo().getTIPO());
+            br.write("2 - " + type.getTIPO());
             br.newLine();
-            br.write("3 - "+pet.getSexo().getSEXO());
+            br.write("3 - " + sex.getSEXO());
             br.newLine();
-            br.write("4 - " +pet.getEndereco().toString());
+            br.write("4 - " + endereco.toString());
             br.newLine();
-            br.write("5 - " +pet.getIdade().toString());
+            br.write("5 - " + age.toString());
             br.newLine();
-            br.write("6 - " +pet.getPeso().toString());
+            br.write("6 - " + weight.toString());
             br.newLine();
-            br.write("7 - "+pet.getRaca());
+            br.write("7 - " + race);
             br.flush();
+            archivesPet.add(filePet.toString());
             System.out.println("Pet criado com sucesso!");
+            System.out.println(archivesPet);
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
     }
+
 
 
 }
--- src/services/PetService.java
+++ src/services/PetService.java
@@ -1,4 +1,13 @@
 package services;
 
+import entities.Pet;
+import repository.PetRepository;
+
+import java.io.IOException;
+
 public class PetService {
+    public static void save(Pet pet) throws IOException {
+        PetRepository.save(pet);
+    }
+
 }
